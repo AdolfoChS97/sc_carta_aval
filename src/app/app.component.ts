4dcfb101bdc51153806c5bb9004dc39f54053453
Josue ortiz <ortizjd0036@gmail.com>
2024-02-07 18:05:59 -0400

    feat: add interface and mock of sickness and clinic, the front list with for cicle all sicknes and clinic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RequesterDataComponent } from './components/requester-data/requester-data.component';
+import { DatePolicyApplicantComponent } from './components/date-policy-applicant/date-policy-applicant.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RequesterDataComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    RequesterDataComponent,
+    DatePolicyApplicantComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
