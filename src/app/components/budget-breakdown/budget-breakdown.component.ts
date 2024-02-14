5e20c86c13d16be8ee71a3d0a5c7d075e91d7095
Josue ortiz <ortizjd0036@gmail.com>
2024-02-14 13:03:22 -0400

    fix: input expenses disable

--- src/app/components/budget-breakdown/budget-breakdown.component.ts
+++ src/app/components/budget-breakdown/budget-breakdown.component.ts
@@ -1,25 +1,29 @@
-import { Component, PipeTransform } from '@angular/core';
-import { MatCommonModule } from '@angular/material/core';
+import { Component} from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-budget-breakdown',
   standalone: true,
-  imports: [
-    MatCommonModule,
-    MatFormFieldModule,
-    FlexLayoutModule,
-    MatInputModule,
-    MatButtonModule
-  ],
+  imports: [CommonModule, FlexLayoutModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './budget-breakdown.component.html',
   styleUrl: './budget-breakdown.component.css'
 })
 
 export class BudgetBreakdownComponent{
+form :FormGroup = new FormGroup({})
 
+constructor(){
+  this.form.addControl('clinicExpenses' , new FormControl(''));
+  this.form.controls['clinicExpenses'].disable();
 
+  this.form.addControl('honoraryExpenses' , new FormControl(''));
+  this.form.controls['honoraryExpenses'].disable();
+
+  this.form.addControl('totalExpenses' , new FormControl(''));
+  this.form.controls['totalExpenses'].disable();
+}
 }
