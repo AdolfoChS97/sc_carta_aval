1633be8edb7e009a8610873d856ab3c735cd7c60
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-02-19 15:25:24 -0400

    feat: update endorsement component

--- src/app/components/endorsement/endorsement.component.ts
+++ src/app/components/endorsement/endorsement.component.ts
@@ -3,6 +3,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { SICKNESS } from '../../../mock/sickness';
+import { CLINICAS } from '../../../mock/clinic';
 
 @Component({
   selector: 'app-endorsement',
@@ -12,13 +15,24 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule, 
     ReactiveFormsModule, 
     MatFormFieldModule, 
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
   ],
   templateUrl: './endorsement.component.html',
   styleUrl: './endorsement.component.css'
 })
 export class EndorsementComponent {
   form: FormGroup = new FormGroup({});
+
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  sickness = SICKNESS;
+  clinics = CLINICAS;
+
 
   constructor() {
     this.form.addControl('name', new FormControl(''));
