9dadf136370e4c15420304818ed3d8308aeaf62e
Adolfo Andres Chafardett Silva <adolfo1997a@gmail.com>
2024-03-08 20:25:46 +0000

    Feat/filter by rif

---
+++ src/app/mappers/policies.ts
@@ -0,0 +1,27 @@
+import { GetPoliciesByIdResponse, Policies } from "../types/Policies";
+
+export function getPoliciesByIdMapper(policies: GetPoliciesByIdResponse): Map<string, string> {
+    try {
+        let result = new Map();
+
+        result.set('name', '');
+        result.set('policies', []);
+
+        const { code, data } = policies;
+       
+        if(code) {
+            return result;
+        }
+
+        data?.forEach((value: Policies) => {
+            const { NUM_POLIZA, NOMBRE_TITULAR, APELLIDO_TITULAR, COD_POLIZA } = value
+            result.set('name', `${NOMBRE_TITULAR} ${APELLIDO_TITULAR}`);
+            result.set('policies', [...result.get('policies'), `${NUM_POLIZA}-${COD_POLIZA}`])
+        })
+
+        return result;
+    } catch (e) {
+        console.log('something happened', e);
+        throw e;
+    }
+}
